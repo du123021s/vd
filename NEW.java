1e9a0534e73aeda6dfb9a9c76f3986cb44cb6c04
du123021s <102853848+du123021s@users.noreply.github.com>
2023-03-24 21:46:43 +0700

    QQQQQQQQQQQQQQQQQQQQQUA

--- NEW.java
+++ NEW.java
@@ -4,5 +4,6 @@ public class NEW extends JFrame {
     public void show(){
         this.setSize(600,400);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
     }
 }
