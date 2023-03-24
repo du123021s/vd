baf15656ea7796c8ca698186c49ef43bb790bed3
du123021s <102853848+du123021s@users.noreply.github.com>
2023-03-24 21:49:54 +0700

    WWWWWWWWWWWWWWU

--- NEW.java
+++ NEW.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 
 public class NEW extends JFrame {
+    public NEW(){
+        
+    }
     public void show(){
         this.setSize(600,400);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
